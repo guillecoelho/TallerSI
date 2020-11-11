6aa54bad605d885f6a3dd072722d48ac8431aed7
ElBoDeLaJusticia <guillecoe2015@gmail.com>
2020-11-10 23:48:24 -0300

    Comienzo nuevo taller
    Pagina web con un iframe a otra pagina.
    Hay que solucionar error de refuse.

---
+++ TallerClick/web/src/app.js
@@ -0,0 +1,21 @@
+var xmlHTTP = new XMLHttpRequest();
+try {
+	xmlHTTP.open('GET', 'conf.xml', false);
+	xmlHTTP.send(null);
+} catch (e) {
+	window.alert('Unable to load the requested file.');
+}
+
+window.onload = () => {
+	var link = 'https://www.phpbb.com/';
+	var iframe = document.createElement('iframe');
+	iframe.frameBorder = 0;
+	iframe.width = '75%';
+	iframe.height = '450px';
+	iframe.style = 'position: absolute; left: 13%; margin-top: 33px;';
+	iframe.id = 'page';
+	iframe.setAttribute('src', link);
+	document.getElementById('phpbb').appendChild(iframe);
+};
+
+console.log('Esta funcionando');
